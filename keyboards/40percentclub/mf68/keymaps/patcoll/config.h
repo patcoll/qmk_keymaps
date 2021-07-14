07c367a5060a9d751c7bd2768c19bea8c012ce98
Patrick Collins <pat@burned.com>
2021-07-14 15:30:35 -0400

    Simplify MF68 keymap

--- keyboards/40percentclub/mf68/keymaps/patcoll/config.h
+++ keyboards/40percentclub/mf68/keymaps/patcoll/config.h
@@ -1,5 +1,5 @@
 #define TAPPING_TERM 200            // How long before a tap becomes a hold, if set above 500, a key tapped during the tapping term will turn it into a hold too
-#define TAPPING_TERM_PER_KEY        // Enables handling for per key TAPPING_TERM settings
+// #define TAPPING_TERM_PER_KEY        // Enables handling for per key TAPPING_TERM settings
 // #define PERMISSIVE_HOLD             // Makes tap and hold keys trigger the hold if another key is pressed before releasing, even if it hasn't hit the TAPPING_TERM. See Permissive Hold for details
 
 // Mouse key speed and acceleration.
--- keyboards/40percentclub/mf68/keymaps/patcoll/keymap.c
+++ keyboards/40percentclub/mf68/keymaps/patcoll/keymap.c
@@ -12,34 +12,6 @@
 #define BR_FWD LALT(KC_RBRC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-	[0] = LAYOUT_68_ansi(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_HOME, KC_PGUP,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_END, KC_PGDN,
-        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
-    ),
-	[1] = LAYOUT_68_ansi(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,     KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_VOLU, KC_HOME,
-        _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, BL_STEP, _______, KC_VOLD, KC_END,
-        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, KC_MUTE,
-        _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT
-    ),
-	[2] = LAYOUT_68_ansi(
-        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_BSPC, KC_VOLU, KC_HOME,
-        _______, _______, _______, KC_UP, _______, _______, _______, KC_7, KC_8, KC_9, _______, _______, _______, _______, KC_VOLD, KC_END,
-        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_4, KC_5, KC_6, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_0, KC_1, KC_2, KC_3, _______, _______, KC_MUTE,
-        _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT
-    )
-    */
-
-
-
-
-
   [0] = LAYOUT_68_ansi(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_MPLY, KC_PGUP,
     TAB_FN,  Q_FN,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, BSLS_FN,          KC_DEL,  KC_PGDN,
@@ -62,17 +34,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______,                            KC_BTN1,                            _______, _______, KC_BTN1,          KC_MS_L, KC_MS_D, KC_MS_R
   )
 };
-
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        /* case SFT_T(KC_SPC): */
-        /*     return TAPPING_TERM + 1250; */
-        /* case LT(1, KC_GRV): */
-        /*     return 130; */
-        case SPC_MET:
-            /* return 130; */
-            return TAPPING_TERM + 1250;
-        default:
-            return TAPPING_TERM;
-    }
-}
