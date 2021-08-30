7c8f15f770d2d0cf46bc3025d8485f5b97329b46
Patrick Collins <pat@burned.com>
2021-08-30 13:56:52 -0400

    More customizations for fc600c

--- keyboards/fc660c/keymaps/patcoll/keymap.c
+++ keyboards/fc660c/keymaps/patcoll/keymap.c
@@ -34,23 +34,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LT4_Q   LT(_MOUSE, KC_Q)
 
 #define CTL_Q   LCTL_T(KC_Q)
+#define CTL_1   LCTL_T(KC_1)
 #define ALT_SLS   LALT_T(KC_SLSH)
+
+#define LT2_3   LT(2, KC_3)
+#define LT2_4   LT(2, KC_4)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-        KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,     KC_NO,
-        SFT_TAB,CTL_Q,  LT1_W,  KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,     KC_DEL,
+        KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,     KC_PGUP,
+        SFT_TAB,CTL_Q,  LT1_W,  KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,     KC_PGDN,
         CTL_ESC,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,     KC_ENT,
-        SFT_ESC,LT1_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, ALT_SLS,KC_RSFT,     KC_UP,
+        SFT_ESC,LT1_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,     KC_UP,
         KC_LCTL,KC_LGUI,KC_LALT,          KC_SPC,                       KC_RALT,KC_RCTL,MO(1),      KC_LEFT,KC_DOWN,KC_RGHT
     ),
   [1] = LAYOUT(
-        KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,     _______,
-        _______,LCTL_T(KC_1),   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,_______,_______,     _______,
-        _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______,_______,     _______,
-        _______,_______,_______,_______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END,_______,_______,     KC_VOLU,
-        RESET,  _______,_______,                _______,                _______,_______,MO(1),       _______,KC_VOLD,KC_MPLY
-    )
+        KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,     KC_PSCR,
+        _______,CTL_1,  KC_2,   LT2_3,  LT2_4,  KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,_______,_______,     _______,
+        _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,TO(2),  _______,     _______,
+        _______,_______,_______,_______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END,_______,_______,      KC_PGUP,
+        RESET,  _______,_______,                _______,                _______,_______,MO(1),       KC_HOME,KC_PGDN,KC_END
+    ),
+  [2] = LAYOUT(
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,
+        _______,KC_LCTL,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,
+        _______,_______,_______,_______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,     _______,
+        _______,_______,_______,_______,TO(0),  _______,KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,_______,_______,     KC_VOLU,
+        _______,_______,KC_BTN1,                KC_BTN1,                KC_BTN2,_______,_______,     _______,KC_VOLD,KC_MPLY
+    ),
 };
 
 #ifdef COMBO_ENABLE
