9e2f4e62fa3c4cc9002284b87752ab058cd0a940
Patrick Collins <pat@burned.com>
2021-10-05 11:53:45 -0400

    Remove unused

--- keyboards/fc660c/keymaps/patcoll/keymap.c
+++ keyboards/fc660c/keymaps/patcoll/keymap.c
@@ -51,28 +51,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, KC_BTN1, KC_BTN1,                                     KC_BTN2, _______, _______,          _______, _______, _______
 ),
 };
-
-/* const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { */
-/*   [_QWERTY] = LAYOUT( */
-/*         KC_GRV, ________________NUMBER_LEFT________________,   ________________NUMBER_RIGHT_______________,   KC_MINS,KC_EQL, KC_BSPC,     KC_PGUP, */
-/*         SFT_TAB,CTL_Q,  NAV_W,  KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,     KC_PGDN, */
-/*         CTL_ESC,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,     KC_ENT, */
-/*         KC_LSFT,NAV_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,     KC_UP, */
-/*         KC_LCTL,KC_LGUI,KC_LALT,          KC_SPC,                       KC_RALT,KC_RCTL,KC_HYPR,     KC_LEFT,KC_DOWN,KC_RGHT */
-/*     ), */
-/*   [_NAV] = LAYOUT( */
-/*         _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,     KC_PSCR, */
-/*         KC_LSFT,CTL_1,  KC_2,   CS_3,   MOUSE_4,  KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   _______,_______,_______,     _______, */
-/*         KC_LCTL,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,TO(_MOUSE),  _______,     _______, */
-/*         KC_LSFT,KC_LCTL,KC_LGUI,KC_LALT,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END, _______,_______,      KC_PGUP, */
-/*         RESET,  _______,_______,                _______,                _______,_______,_______,       KC_HOME,KC_PGDN,KC_END */
-/*     ), */
-/*   [_MOUSE] = LAYOUT( */
-/*         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______, */
-/*         _______,KC_LCTL,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______, */
-/*         _______,_______,_______,_______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,     _______, */
-/*         _______,_______,_______,_______,TO(_QWERTY),  _______,KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,_______,_______,     KC_VOLU, */
-/*         _______,_______,KC_BTN1,                KC_BTN1,                KC_BTN2,_______,_______,     _______,KC_VOLD,KC_MPLY */
-/*     ), */
-/* }; */
-
