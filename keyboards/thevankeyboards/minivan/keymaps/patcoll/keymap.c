f4bf2cef680fc7b3383b6e56e355234332f18ae2
Patrick Collins <pat@burned.com>
2021-10-22 19:58:54 -0400

    Tweak minivan pcb layout to have ctl/ent instead of plain enter

--- keyboards/thevankeyboards/minivan/keymaps/patcoll/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/patcoll/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   SFT_TAB, _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_BSPC,
   CTL_ESC, _________________QWERTY_L2_________________, _________________QWERTY_R2_________________, SFT_QUOT,
   KC_LSFT, _________________QWERTY_L3_________________, _________________QWERTY_R3_ARROWS__________, SFT_SLSH,
-  KC_LCTL, KC_LGUI, KC_LALT, SFT_SPC,                   RSE_ENT, KC_LCTL, __QWERTY_R3_ARROW_KEYS__
+  KC_LCTL, KC_LGUI, KC_LALT, SFT_SPC,                   RSE_ENT, CTL_ENT, __QWERTY_R3_ARROW_KEYS__
 ),
 
 [_NAV] = LAYOUT_wrapper(
