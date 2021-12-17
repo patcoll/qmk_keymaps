73279db5f8842479beb3b1f8da353778aa4f3e38
Patrick Collins <pat@burned.com>
2021-12-17 15:35:57 -0500

    Remove unused code

--- users/patcoll/patcoll.c
+++ users/patcoll/patcoll.c
@@ -3,36 +3,14 @@
 #include "g/keymap_combo.h"
 #endif
 
-bool is_win_switch_active = false;
 bool is_alt_tab_active = false;
-bool is_tab_switch_active = false;
-#ifdef ENCODER_ENABLE
-bool is_alt_tab_enc_active = false;
-uint16_t alt_tab_enc_timer = 0;
-#endif
 
 void matrix_scan_user(void) {
   // End fancy nav switching if the NAV layer has been deactivated.
-  if (is_win_switch_active && !IS_LAYER_ON(_NAV)) {
-    unregister_code(KC_LGUI);
-    is_win_switch_active = false;
-  }
   if (is_alt_tab_active && !IS_LAYER_ON(_NAV)) {
     unregister_code(KC_LALT);
     is_alt_tab_active = false;
   }
-  if (is_tab_switch_active && !IS_LAYER_ON(_NAV)) {
-    unregister_code(KC_LCTL);
-    is_tab_switch_active = false;
-  }
-# ifdef ENCODER_ENABLE
-  if (is_alt_tab_enc_active) {
-    if (timer_elapsed(alt_tab_enc_timer) > 750) {
-      unregister_code(KC_LALT);
-      is_alt_tab_enc_active = false;
-    }
-  }
-# endif
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -62,14 +40,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       // Super Alt-Tab
       case ALT_TAB:
       case SALTTAB:
-        if (is_win_switch_active) {
-          is_win_switch_active = false;
-          unregister_code(KC_LGUI);
-        }
-        if (is_tab_switch_active) {
-          is_tab_switch_active = false;
-          unregister_code(KC_LCTL);
-        }
         if (!is_alt_tab_active) {
           is_alt_tab_active = true;
           register_code(KC_LALT);
