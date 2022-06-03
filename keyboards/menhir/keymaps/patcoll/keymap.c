7368ae5d2145cc77bc300d81902ecf8c6cba60e9
Patrick Collins <pat@burned.com>
2022-06-03 10:52:06 -0400

    remove arrows config from menhir

--- keyboards/menhir/keymaps/patcoll/keymap.c
+++ keyboards/menhir/keymaps/patcoll/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_wrapper(
   SFT_TAB, _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, CS_BSPC,
   CTL_ESC, _________________QWERTY_L2_________________, _________________QWERTY_R2_________________, SFT_QUOT,
-  KC_LSFT, _________________QWERTY_L3_________________, _________________QWERTY_R3_ARROWS__________,
+  KC_LSFT, _________________QWERTY_L3_________________, _________________QWERTY_R3_________________,
   KC_LGUI, KC_LALT, SFT_SPC, SFT_SPC,                   RSE_ENT, KC_LCTL, KC_HYPR
 ),
 
