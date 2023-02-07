8d15c8cb4e98dfc6c957ff59b44c0b322c3c690e
Patrick Collins <pat@burned.com>
2023-02-07 13:10:23 -0500

    update embrace code

--- firmware/embrace/embrace.c
+++ firmware/embrace/embrace.c
@@ -13,3 +13,14 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
 void led_set_kb(uint8_t usb_led) {
   /* led_set_user(usb_led); */
 }
+
+bool led_update_kb(led_t led_state) {
+  return false;
+
+  bool res = led_update_user(led_state);
+
+  /* if (res) { */
+  /* } */
+
+  return res;
+}
