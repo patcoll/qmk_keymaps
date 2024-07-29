d52213194b679d1b8b360c309af4ab2d9030c052
Patrick Collins <pat@burned.com>
2024-07-28 20:33:00 -0400

    remove unnecessary overrides

--- firmware/nachie/syndrome/chconf.h
+++ firmware/nachie/syndrome/chconf.h
@@ -7,21 +7,6 @@
 
 #include_next <chconf.h>
 
-#undef CH_CFG_SMP_MODE
-#define CH_CFG_SMP_MODE                     TRUE
-
-#undef CH_CFG_ST_RESOLUTION
-#define CH_CFG_ST_RESOLUTION                32
-
-#undef CH_CFG_ST_FREQUENCY
-#define CH_CFG_ST_FREQUENCY                 1000000
-
-#undef CH_CFG_INTERVALS_SIZE
-#define CH_CFG_INTERVALS_SIZE               32
-
-#undef CH_CFG_TIME_TYPES_SIZE
-#define CH_CFG_TIME_TYPES_SIZE              32
-
 #undef CH_CFG_ST_TIMEDELTA
 #define CH_CFG_ST_TIMEDELTA                 100
 
