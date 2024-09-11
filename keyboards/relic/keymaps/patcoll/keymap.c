f38ff83630a8011fc6ce23b76c01db3549f98637
Patrick Collins <pat@burned.com>
2024-09-11 11:00:17 -0400

    turn off top right key in relic 11u

--- keyboards/relic/keymaps/patcoll/keymap.c
+++ keyboards/relic/keymaps/patcoll/keymap.c
@@ -4,7 +4,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_wrapper(
-  _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_BSPC,
+  _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_NO,
   _________________QWERTY_L2_________________, _________________QWERTY_R2_________________,
   _________________QWERTY_L3_________________, _________________QWERTY_R3_________________,
   KC_LGUI, KC_LALT, KC_LALT, SFT_SPC, SFT_SPC, RSE_ENT, KC_LCTL, KC_HYPR, KC_MEH
