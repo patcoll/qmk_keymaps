bc049d839449cdc1fb887f7d4cebee88e78d027d
Patrick Collins <pat@burned.com>
2024-09-11 11:01:53 -0400

    tweak combo timing

--- users/patcoll/combos.c
+++ users/patcoll/combos.c
@@ -8,10 +8,28 @@
 uint16_t get_combo_term(uint16_t index, combo_t *combo) {
   // or with combo index, i.e. its name from enum.
   switch (index) {
+    // Make these combos slightly easier to trigger.
+    //
+    // Do this for any combo letter combinations that may NOT be more prevalent
+    // in everyday typing.
+    case jk_combo:
+      return COMBO_TERM + 8;
+
+    case mk_combo:
+      return COMBO_TERM + 3;
+
+    // Make these combos slightly harder to trigger. In other words, you must
+    // trigger all keys together more quickly.
+    //
+    // Do this for any combo letter combinations that may be more prevalent in
+    // everyday typing.
     case as_combo:
     case ui_combo:
     case io_combo:
     case uo_combo:
+
+    case dv_combo:
+    case db_combo:
 
 #ifdef PATCOLL_BIG_BAR_COMBOS
     case dsspc_combo:
@@ -23,6 +41,12 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo) {
     case zsspc_combo:
 #endif
       return COMBO_TERM - 12;
+
+    // Make this much harder to trigger, since it's a common letter combo.
+    case af_combo:
+    case av_combo:
+      return COMBO_TERM - 17;
+
   }
 
   return COMBO_TERM;
